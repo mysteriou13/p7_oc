bf64f4202dd6af230c9f861c8087cea950e0b871
mysteriou <elio07@hotmail.fr>
2023-05-05 09:00:22 +0200

    ajout lien vers logement dans le composant acceuil

--- src/composant/acceuil.js
+++ src/composant/acceuil.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+
+
 import jsonData from '../logements.json';
+
 
 function Acceuil() {
   const [data] = useState(jsonData);
@@ -7,15 +12,19 @@ function Acceuil() {
   const listItems = data.map((logement) =>
 <div className = "card_acceuil" key={logement.id}>
 
+<Link to ={`/logement?id=${logement.id}`}>
 
 <div className = "div_image_dacceuil">
 <img  className = "image_acceuil" src={logement.cover} alt={logement.title} />
 </div>
 
-<div className = "title_logement"> {logement.title}</div>
+<div className = "title_logement"> {logement.title} </div>
+
+</Link>
 
 </div>
 );
+
 
   return (
 
