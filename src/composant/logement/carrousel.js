dd5665e732962107663c265744bd5cadb6132c8e
mysteriou <elio07@hotmail.fr>
2023-05-08 05:53:53 +0200

    ajout fleche au composant carrousel

--- src/composant/logement/carrousel.js
+++ src/composant/logement/carrousel.js
@@ -1,35 +1,56 @@
 import React, { useRef, useState } from "react";
 
+import image from "../../image/Vector_left.png";
+
 function Carrousel(props) {
   const carrouselRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNextClick = () => {
-    setCurrentIndex(currentIndex + 1);
+    if (currentIndex === props.person.pictures.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
   };
 
   const handlePrevClick = () => {
-    setCurrentIndex(currentIndex - 1);
+    if (currentIndex === 0) {
+      setCurrentIndex(props.person.pictures.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
   };
 
   return (
-    <div>
-      <button className="carrousel-button" onClick={handlePrevClick} disabled={currentIndex === 0}>
-        Image précédente
-      </button>
-      <button className="carrousel-button" onClick={handleNextClick} disabled={currentIndex === props.person.pictures.length - 1}>
-        Image suivante
-      </button>
+    <div className="div_carrousel">
+      <div
+        className=" carrousel_left carrousel-button"
+        onClick={handlePrevClick}
+      >
+        <img className = "fleche_left_carrousel" src = {image} />
+      </div>
 
       <div className="carrousel" ref={carrouselRef}>
         {props.person.pictures.map((item, index) => (
-          <img key={index} src={item} style={{ display: index === currentIndex ? "block" : "none" }} />
+          <img
+            key={index}
+            src={item}
+            className="img_carrousel"
+            style={{ display: index === currentIndex ? "block" : "none" }}
+          />
         ))}
+      </div>
+
+      <div
+        className="carrousel_right carrousel-button"
+        onClick={handleNextClick}
+       
+      >
+         <img className = "fleche_right_carrousel" src = {image} />
       </div>
     </div>
   );
 }
-
-
 
 export default Carrousel;
