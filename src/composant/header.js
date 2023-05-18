944c76604fe5bdb41bf079f9103396438369eddf
mysteriou <elio07@hotmail.fr>
2023-05-18 08:44:29 +0200

    ajout onclick au link du du menu_nav

--- src/composant/header.js
+++ src/composant/header.js
@@ -1,23 +1,58 @@
 import React from 'react';
-
 import logo from '../image/LOGO.png';
-
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+  const nav_Click = (param) => {
+
+    var id = document.querySelector(param);
+
+   var tab = ["#index", "#propos"];
+
+     for(var a = 0; a<= tab.length-1; a++){
+
+
+        if(tab[a] == param){
+
+          document.querySelector(tab[a]).style.textDecoration = "underline";
+
+        }else{
+
+          document.querySelector(tab[a]).style.textDecoration = "none";
+
+         
+        }
+
+     }
+
+  };
+
   return (
     <header>
-   
-      
-      <div className = "box_nav">
-    <div> <img className = "logo" src  = {logo}/> </div><div> <nav>  
-      
-      <div><Link to="/">Accueil</Link> </div> 
-     <div> <Link to="/propos">À propos</Link> </div>  </nav> </div></div>
-      
+      <div className="box_nav">
+        <div>
+          <img className="logo" src={logo} />
+        </div>
+        <div>
+          <nav>
+            <div>
+              <Link to="/" id = "index" className ='index' onClick={() => nav_Click("#index")}>
+                Accueil
+              </Link>
+            </div>
+            <div>
+              <Link to="/propos" id = "propos" className='propos' onClick={() => nav_Click("#propos")}>
+                À propos
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </div>
     </header>
   );
+
+  
 }
 
 export default Header;
-
