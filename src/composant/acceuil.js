fa7066a24912ac123f47ba8f1de14c5cdbaae921
mysteriou <elio07@hotmail.fr>
2023-05-18 14:47:44 +0200

    ajout fonction  pour enlever les souligne du menu nav au click sur les card logement

--- src/composant/acceuil.js
+++ src/composant/acceuil.js
@@ -7,12 +7,30 @@ import jsonData from '../logements.json';
 
 
 function Acceuil() {
+
+
+  const logement_Click = (param) => {
+
+    var id = document.querySelector(param);
+
+   var tab = ["#index", "#propos"];
+
+     for(var a = 0; a<= tab.length-1; a++){
+
+          document.querySelector(tab[a]).style.textDecoration = "none";
+
+
+     }
+
+  };
+
+
   const [data] = useState(jsonData);
 
   const listItems = data.map((logement) =>
 <div className = "card_acceuil" key={logement.id}>
 
-<Link to ={`/logement?id=${logement.id}`}>
+<Link to ={`/logement?id=${logement.id}`}  onClick={() => logement_Click()}>
 
 <div className = "div_image_dacceuil">
 <img  className = "image_acceuil" src={logement.cover} alt={logement.title} />
